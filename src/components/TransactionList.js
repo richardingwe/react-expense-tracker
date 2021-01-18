6b468514da620b564a141f1e0892744068d93e01
Richard Ingwe <richardingwe5@gmail.com>
2021-01-18 13:17:04 +0100

    added basic components and input state

---
+++ src/components/TransactionList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const TransactionList = () => {
+    return (
+        <>
+            <h3>History</h3>
+            <ul className="list">
+                <li className="minus">
+                    Cash <span>N2000</span><button className="delete-btn">X</button>
+                </li>
+            </ul>
+        </>
+    );
+};
