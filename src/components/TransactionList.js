ca8d42c76d7f1116bbd30b1ffb3d7186433c1cd4
Richard Ingwe <richardingwe5@gmail.com>
2021-01-18 22:04:18 +0100

    added a dispatch function for deleting transactions

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,15 +1,16 @@
 import React, { useContext } from 'react';
 import { GlobalContext } from "../context/GlobalState";
+import { Transaction } from './Transaction';
 
 export const TransactionList = () => {
-    const context = useContext(GlobalContext);
+    const { transactions } = useContext(GlobalContext);
     return (
         <>
             <h3>History</h3>
             <ul className="list">
-                <li className="minus">
-                    Cash <span>N2000</span><button className="delete-btn">X</button>
-                </li>
+                {transactions.map(transaction => (
+                    <Transaction key={transaction.id} transaction={transaction} />
+                ))}
             </ul>
         </>
     );
